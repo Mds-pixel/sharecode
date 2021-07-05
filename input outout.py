8e506199a184f697213855381ccc19c2750f44c5
Mds-pixel <83350566+Mds-pixel@users.noreply.github.com>
2021-07-05 22:39:48 +0700

    Add files via upload

---
+++ input outout.py
@@ -0,0 +1,12 @@
+print("belajar input dan output")
+
+#buat bagian input
+alamat=input("masukan alamat anda disini :")
+nama=input("masukan nama anda disiini :")
+nim=int(input("masukan nim anda disin :"))
+
+#buat bagian output
+print("==========================================================")
+print("alamat anda berada di : " +(alamat))
+print("nama anda adalah : " +(nama))
+print("nim anda adalah : " +str(nim))
